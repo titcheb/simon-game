f92196f1a508f13b090b75f2a462689b200cd781
titcheb <92898342+titcheb@users.noreply.github.com>
2024-03-23 04:12:22 +0100

    Update game.js

--- game.js
+++ game.js
@@ -13,7 +13,15 @@ $(document).keypress(function (e) {
     nextSequence();
     started = true;
   }
+$(document).click(function (e){
+    if (!started) {
 
+      //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+      $("#level-title").text("Level " + level);
+      nextSequence();
+      started = true;
+    }
+  }
 
 });
 $(".btn").click(function () {
